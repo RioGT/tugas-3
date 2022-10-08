5de2b86c5d241e1336ba39dc065793128916430b
[RioGT] <riooctavianus10@gmail.com>
2022-10-08 16:38:52 +0700

    first commit

---
+++ Untitled-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+    
+
+    int x ;
+
+    cout << "Masukan bilangan
+     :";
+    cin >> x;
+
+
+    if (
+        x%2 == 0 )
+    {
+        cout << "genap";
+    }
+    else{
+        cout << "ganjil";
+    }
+    
+}
